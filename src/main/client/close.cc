a360e4e34a2bc48d567a3529be0651cd8d75ce7a
Chris Stivers <chris@aerospike.com>
2014-01-09 13:48:16 -0800

    minor cleanup

--- src/main/client/close.cc
+++ src/main/client/close.cc
@@ -21,11 +21,11 @@
  ******************************************************************************/
 
 extern "C" {
-#include <aerospike/aerospike.h>
-#include <aerospike/aerospike_key.h>
-#include <aerospike/as_config.h>
-#include <aerospike/as_key.h>
-#include <aerospike/as_record.h>
+    #include <aerospike/aerospike.h>
+    #include <aerospike/aerospike_key.h>
+    #include <aerospike/as_config.h>
+    #include <aerospike/as_key.h>
+    #include <aerospike/as_record.h>
 }
 
 #include <node.h>
