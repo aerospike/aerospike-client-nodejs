02df0523b68225e677b97aaa25eb61ec0485cfed
Jan Hecking <jhecking@netgaroo.com>
2016-03-28 18:20:13 +0800

    expose C client's is_connected method to check for connection health
    fixes aerospike/aerospike-client-nodejs#108

--- test/client.js
+++ test/client.js
@@ -22,16 +22,48 @@ const helper = require('./test_helper')
 
 describe('Client', function () {
   describe('Client#isConnected', function () {
-    var client = new Client(Aerospike, helper.config)
-
-    it('returns false if the client is not connected', function () {
-      expect(client.isConnected()).to.be(false)
+    var client = null
+    beforeEach(function () {
+      client = new Client(Aerospike, helper.config)
+    })
+    afterEach(function () {
+      client.close(false)
     })
 
-    it('returns true if the client is connected', function (done) {
-      client.connect(function () {
-        expect(client.isConnected()).to.be(true)
-        done()
+    context('without tender health check', function () {
+      it('returns false if the client is not connected', function () {
+        expect(client.isConnected(false)).to.be(false)
+      })
+
+      it('returns true if the client is connected', function (done) {
+        client.connect(function () {
+          expect(client.isConnected(false)).to.be(true)
+          done()
+        })
+      })
+
+      it('returns false after the connection is closed', function (done) {
+        client.connect(function () {
+          client.close(false)
+          expect(client.isConnected(false)).to.be(false)
+          done()
+        })
+      })
+    })
+
+    context('with tender health check', function () {
+      var orig = null
+      before(function () { orig = client.as_client.isConnected })
+      after(function () { client.as_client.isConnected = orig })
+
+      it("calls the Aerospike C client libraries' isConnected() method", function (done) {
+        client.connect(function () {
+          var tenderHealthCheck = false
+          client.as_client.isConnected = function () { tenderHealthCheck = true; return false }
+          expect(client.isConnected(true)).to.be(false)
+          expect(tenderHealthCheck).to.be(true)
+          done()
+        })
       })
     })
   })
