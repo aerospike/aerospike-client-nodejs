38380a399ecf4c07bfc123b13031024bbb41f67e
Behcet <behcet@dolm.us>
2016-10-10 12:58:57 -0400

    Added Ubuntu 16 support

--- scripts/aerospike-client-c.sh
+++ scripts/aerospike-client-c.sh
@@ -67,7 +67,7 @@ detect_linux()
         return 0
         ;;
 
-      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu15" | "linuxmint17" )
+      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu15" | "ubuntu16" | "linuxmint17" )
         echo "ubuntu12"  "deb"
         return 0
         ;;
