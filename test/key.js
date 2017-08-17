eca522c38fa39abf02d063a38443b4eea1c66585
Jan Hecking <jhecking@netgaroo.com>
2017-08-17 16:17:35 +0800

    fixup! Add Key#equals function to compare two keys

--- test/key.js
+++ test/key.js
@@ -14,6 +14,8 @@
 // limitations under the License.
 // *****************************************************************************
 
+'use strict'
+
 /* global expect, describe, context, it */
 
 const Aerospike = require('../lib/aerospike')
