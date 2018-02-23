6e9cd4260f3ce24a61d1b97d657a3e79f3cd2e75
Jan Hecking <jhecking@netgaroo.com>
2018-02-23 17:42:33 +0800

    Refactor lists tests

--- test/cdt_list.js
+++ test/cdt_list.js
@@ -27,24 +27,7 @@ const ops = Aerospike.operations
 const status = Aerospike.status
 
 describe('client.operate() - CDT List operations', function () {
-  var client = helper.client
-  var key = null
-
-  function setup (record, done) {
-    key = helper.keygen.string(helper.namespace, helper.set, {prefix: 'cdt_list/'})()
-    let meta = { ttl: 600 }
-    let policy = new Aerospike.WritePolicy({
-      exists: Aerospike.policy.exists.CREATE_OR_REPLACE
-    })
-    client.put(key, record, meta, policy, function (err) {
-      if (err) throw err
-      done()
-    })
-  }
-
-  function teardown (done) {
-    client.remove(key, done)
-  }
+  let client = helper.client
 
   class State {
     enrich (name, promise) {
@@ -111,12 +94,6 @@ describe('client.operate() - CDT List operations', function () {
     }
   }
 
-  function assertResultSatisfy (matcher) {
-    return function (state) {
-      return state.resolve(expect(state.result.bins).to.satisfy(matcher, 'operate result'))
-    }
-  }
-
   function assertRecordEql (expected) {
     return function (state) {
       return state.passthrough(client.get(state.key)
@@ -137,23 +114,6 @@ describe('client.operate() - CDT List operations', function () {
     return function (state) {
       return state.passthrough(client.remove(state.key))
     }
-  }
-
-  // Helper method to execute a single operation on the given record and verify:
-  // 1) The results returned by the operation,
-  // 2) The contents of the record after the operation.
-  function verifyOperation (record, op, expectedResult, expectedBinsPostOp, done) {
-    setup(record, function () {
-      client.operate(key, [op], function (err, result) {
-        if (err) throw err
-        expect(result.bins).to.eql(expectedResult)
-        client.get(key, function (err, record) {
-          if (err) throw err
-          expect(record.bins).to.eql(expectedBinsPostOp)
-          teardown(done)
-        })
-      })
-    })
   }
 
   describe('lists.setOrder', function () {
@@ -194,189 +154,194 @@ describe('client.operate() - CDT List operations', function () {
   })
 
   describe('lists.append', function () {
-    it('appends an item to the list and returns the list size', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.append('list', 99)
-      var expectedResult = { list: 6 }
-      var expectedRecord = { list: [1, 2, 3, 4, 5, 99] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('appends an item to the list and returns the list size', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.append('list', 99)))
+        .then(assertResultEql({ list: 6 }))
+        .then(assertRecordEql({ list: [1, 2, 3, 4, 5, 99] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.appendItems', function () {
-    it('appends the items to the list and returns the list size', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.appendItems('list', [99, 100])
-      var expectedResult = { list: 7 }
-      var expectedRecord = { list: [1, 2, 3, 4, 5, 99, 100] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('appends the items to the list and returns the list size', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.appendItems('list', [99, 100])))
+        .then(assertResultEql({ list: 7 }))
+        .then(assertRecordEql({ list: [1, 2, 3, 4, 5, 99, 100] }))
+        .then(cleanup)
     })
 
-    it('returns an error if the value to append is not an array', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.appendItems('list', 99)
-      setup(record, function () {
-        client.operate(key, [operation], function (err, result) {
-          expect(err.code).to.equal(status.ERR_PARAM)
-          teardown(done)
-        })
-      })
+    it('returns an error if the value to append is not an array', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(expectError())
+        .then(operate(lists.appendItems('list', 99)))
+        .then(assertError(status.ERR_PARAM))
+        .then(cleanup)
     })
   })
 
   describe('lists.insert', function () {
-    it('inserts the item at the specified index and returns the list size', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.insert('list', 2, 99)
-      var expectedResult = { list: 6 }
-      var expectedRecord = { list: [1, 2, 99, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('inserts the item at the specified index and returns the list size', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.insert('list', 2, 99)))
+        .then(assertResultEql({ list: 6 }))
+        .then(assertRecordEql({ list: [1, 2, 99, 3, 4, 5] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.insertItems', function () {
-    it('inserts the items at the specified index and returns the list size', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.insertItems('list', 2, [99, 100])
-      var expectedResult = { list: 7 }
-      var expectedRecord = { list: [1, 2, 99, 100, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('inserts the items at the specified index and returns the list size', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.insertItems('list', 2, [99, 100])))
+        .then(assertResultEql({ list: 7 }))
+        .then(assertRecordEql({ list: [1, 2, 99, 100, 3, 4, 5] }))
+        .then(cleanup)
     })
 
-    it('returns an error if the value to insert is not an array', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.insertItems('list', 2, 99)
-      setup(record, function () {
-        client.operate(key, [operation], function (err, result) {
-          expect(err.code).to.equal(status.ERR_PARAM)
-          teardown(done)
-        })
-      })
+    it('returns an error if the value to insert is not an array', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(expectError())
+        .then(operate(lists.insertItems('list', 2, 99)))
+        .then(assertError(status.ERR_PARAM))
+        .then(cleanup)
     })
   })
 
   describe('lists.pop', function () {
-    it('removes the item at the specified index and returns it', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.pop('list', 2)
-      var expectedResult = { list: 3 }
-      var expectedRecord = { list: [1, 2, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes the item at the specified index and returns it', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.pop('list', 2)))
+        .then(assertResultEql({ list: 3 }))
+        .then(assertRecordEql({ list: [1, 2, 4, 5] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.popRange', function () {
-    it('removes the items at the specified range and returns them', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.popRange('list', 2, 2)
-      var expectedResult = { list: [3, 4] }
-      var expectedRecord = { list: [1, 2, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes the items at the specified range and returns them', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.popRange('list', 2, 2)))
+        .then(assertResultEql({ list: [3, 4] }))
+        .then(assertRecordEql({ list: [1, 2, 5] }))
+        .then(cleanup)
     })
 
-    it('removes and returns all items starting from the specified index if count is not specified', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.popRange('list', 2)
-      var expectedResult = { list: [3, 4, 5] }
-      var expectedRecord = { list: [1, 2] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes and returns all items starting from the specified index if count is not specified', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.popRange('list', 2)))
+        .then(assertResultEql({ list: [3, 4, 5] }))
+        .then(assertRecordEql({ list: [1, 2] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.remove', function () {
-    it('removes the item at the specified index and returns the number of items removed', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.remove('list', 2)
-      var expectedResult = { list: 1 }
-      var expectedRecord = { list: [1, 2, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes the item at the specified index and returns the number of items removed', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.remove('list', 2)))
+        .then(assertResultEql({ list: 1 }))
+        .then(assertRecordEql({ list: [1, 2, 4, 5] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.removeRange', function () {
-    it('removes the items in the specified range and returns the number of items removed', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.removeRange('list', 2, 2)
-      var expectedResult = { list: 2 }
-      var expectedRecord = { list: [1, 2, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes the items in the specified range and returns the number of items removed', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.removeRange('list', 2, 2)))
+        .then(assertResultEql({ list: 2 }))
+        .then(assertRecordEql({ list: [1, 2, 5] }))
+        .then(cleanup)
     })
 
-    it('removes all items starting from the specified index and returns the number of items removed if count is not specified', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.removeRange('list', 2)
-      var expectedResult = { list: 3 }
-      var expectedRecord = { list: [1, 2] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes all items starting from the specified index and returns the number of items removed if count is not specified', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.removeRange('list', 2)))
+        .then(assertResultEql({ list: 3 }))
+        .then(assertRecordEql({ list: [1, 2] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.clear', function () {
-    it('removes all elements from the list', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.clear('list')
-      var expectedResult = { }
-      var expectedRecord = { list: [] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes all elements from the list', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.clear('list')))
+        .then(assertRecordEql({ list: [] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.set', function () {
-    it('sets the item at the specified index', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.set('list', 2, 99)
-      var expectedResult = { }
-      var expectedRecord = { list: [1, 2, 99, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('sets the item at the specified index', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.set('list', 2, 99)))
+        .then(assertRecordEql({ list: [1, 2, 99, 4, 5] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.trim', function () {
-    it('removes all elements not within the specified range and returns the number of elements removed', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.trim('list', 1, 3)
-      var expectedResult = { list: 2 }
-      var expectedRecord = { list: [2, 3, 4] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('removes all elements not within the specified range and returns the number of elements removed', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.trim('list', 1, 3)))
+        .then(assertResultEql({ list: 2 }))
+        .then(assertRecordEql({ list: [2, 3, 4] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.get', function () {
-    it('returns the item at the specified index', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.get('list', 2)
-      var expectedResult = { list: 3 }
-      var expectedRecord = { list: [1, 2, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('returns the item at the specified index', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.get('list', 2)))
+        .then(assertResultEql({ list: 3 }))
+        .then(cleanup)
     })
 
-    it('should return an error if the index is out of bounds', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.get('list', 99)
-      setup(record, function () {
-        client.operate(key, [operation], function (err, result) {
-          expect(err.code).to.equal(status.ERR_REQUEST_INVALID)
-          teardown(done)
-        })
-      })
+    it('should return an error if the index is out of bounds', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(expectError())
+        .then(operate(lists.get('list', 99)))
+        .then(assertError(status.ERR_REQUEST_INVALID))
+        .then(cleanup)
     })
   })
 
   describe('lists.getRange', function () {
-    it('returns the items in the specified range', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.getRange('list', 1, 3)
-      var expectedResult = { list: [2, 3, 4] }
-      var expectedRecord = { list: [1, 2, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('returns the items in the specified range', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.getRange('list', 1, 3)))
+        .then(assertResultEql({ list: [2, 3, 4] }))
+        .then(cleanup)
     })
 
-    it('returns all items starting at the specified index if count is not specified', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.getRange('list', 1)
-      var expectedResult = { list: [2, 3, 4, 5] }
-      var expectedRecord = { list: [1, 2, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('returns all items starting at the specified index if count is not specified', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.getRange('list', 1)))
+        .then(assertResultEql({ list: [2, 3, 4, 5] }))
+        .then(cleanup)
     })
   })
 
@@ -387,30 +352,32 @@ describe('client.operate() - CDT List operations', function () {
       }
     })
 
-    it('increments the element at the specified index and returns the final value', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.increment('list', 1, 3)
-      var expectedResult = { list: 5 }
-      var expectedRecord = { list: [1, 5, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('increments the element at the specified index and returns the final value', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.increment('list', 1, 3)))
+        .then(assertResultEql({ list: 5 }))
+        .then(assertRecordEql({ list: [1, 5, 3, 4, 5] }))
+        .then(cleanup)
     })
 
-    it('increments the element at the specified index by one and returns the final value', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.increment('list', 2)
-      var expectedResult = { list: 4 }
-      var expectedRecord = { list: [1, 2, 4, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('increments the element at the specified index by one and returns the final value', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.increment('list', 2)))
+        .then(assertResultEql({ list: 4 }))
+        .then(assertRecordEql({ list: [1, 2, 4, 4, 5] }))
+        .then(cleanup)
     })
   })
 
   describe('lists.size', function () {
-    it('returns the element count', function (done) {
-      var record = { list: [1, 2, 3, 4, 5] }
-      var operation = lists.size('list')
-      var expectedResult = { list: 5 }
-      var expectedRecord = { list: [1, 2, 3, 4, 5] }
-      verifyOperation(record, operation, expectedResult, expectedRecord, done)
+    it('returns the element count', function () {
+      return initState()
+        .then(createRecord({ list: [1, 2, 3, 4, 5] }))
+        .then(operate(lists.size('list')))
+        .then(assertResultEql({ list: 5 }))
+        .then(cleanup)
     })
   })
 })
