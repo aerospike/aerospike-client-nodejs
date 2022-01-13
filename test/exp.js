3cb3debb820f76accdacb1f39f2b93fb0f503d93
Jan Hecking <jhecking@aerospike.com>
2022-01-13 10:12:48 +0800

    fixup! Implement list append exp

--- test/exp.js
+++ test/exp.js
@@ -255,7 +255,7 @@ describe('Aerospike.expressions', function () {
       it('appends an element at the end of the list', async function () {
         const key = await createRecord({ values: [83, 39, 49] })
 
-        const result = await applyExp(key, exp.lists.append(exp.binList('values'), exp.int(2)))
+        const result = await applyExp(key, 'values', exp.lists.append(exp.binList('values'), exp.int(2)))
         expect(result).to.eql([83, 39, 49, 2])
       })
     })
