58dd865da23c4620ece6e5243de6b64c1f3a401d
rpandian-spike <rpandian@aerospike.com>
2022-05-31 15:06:44 -0700

    add batch type js enums

--- test/batch_write.js
+++ test/batch_write.js
@@ -22,7 +22,7 @@
 const Aerospike = require('../lib/aerospike')
 const helper = require('./test_helper')
 // const util = require('util')
-const batchType = Aerospike.batchType
+const batchType = Aerospike.batch_type
 
 const op = Aerospike.operations
 const GeoJSON = Aerospike.GeoJSON
