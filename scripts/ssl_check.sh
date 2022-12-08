94b426e89938b0a2a9d7d4b83ce38304d8432e13
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2022-12-08 09:59:52 -0700

    added postinstall check for correct architecture

--- scripts/ssl_check.sh
+++ scripts/ssl_check.sh
@@ -5,12 +5,20 @@ libre=${text[0]};
 osn=${text[1]::-1};
 echo $osn;
 echo $libre;
+archType=$(uname -m)
+echo $archType
 if [[ ($osn =~ 1*) || ($libre == "LibreSSL")]]; then
-	cd lib/binding/openssl@1;
-	for FILE in */; do rm -rf "../$FILE" && cp -r $FILE  "../$FILE"; done
+        cd lib/binding/openssl@1;
+        for FILE in */; do rm -rf "../$FILE" && cp -r $FILE  "../$FILE"; done
+        cd ../../..;
 else
-	cd lib/binding/openssl@3;
-	for FILE in */; do rm -rf "../$FILE" && cp -r $FILE  "../$FILE"; done
+        cd lib/binding/openssl@3;
+        for FILE in */; do rm -rf "../$FILE" && cp -r $FILE  "../$FILE"; done
+        cd ../../..;
 fi
-cd ../../..;
-./node_modules/.bin/node-pre-gyp install --fallback-to-build
+if [[ ($archType == "aarch64")]]; then
+        ./node_modules/.bin/node-pre-gyp install --fallback-to-build target_arch=aarch64
+else
+        ./node_modules/.bin/node-pre-gyp install --fallback-to-build
+fi
+
