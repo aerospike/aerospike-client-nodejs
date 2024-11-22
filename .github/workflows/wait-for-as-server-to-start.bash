cf38891a6e251e4e76052b588c82f363770c81b4
Dominic Pelini <dpelini@aerospike.com>
2024-11-22 05:24:59 -0700

    pipeline improvements

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -6,6 +6,7 @@ set -o pipefail
 
 container_name=$1
 is_security_enabled=$2
+is_strong_consistency_enabled=$3
 
 if [[ $is_security_enabled == true ]]; then
     # We need to pass credentials to asinfo if server requires it
@@ -38,7 +39,14 @@ done
 while true; do
     echo "Waiting for server to stabilize (i.e return a cluster key)..."
     # We assume that when an ERROR is returned, the cluster is not stable yet (i.e not fully initialized)
-    if docker exec "$container_name" asinfo $user_credentials -v cluster-stable 2>&1 | (! grep -qE "^ERROR"); then
+    cluster_stable_info_cmd="cluster-stable"
+    if [[ $is_strong_consistency_enabled == true ]]; then
+        # The Dockerfile uses a roster from a previously running Aerospike server in a Docker container
+        # When we reuse this roster, the server assumes all of its partitions are dead because it's running on a new
+        # storage device.
+        cluster_stable_info_cmd="$cluster_stable_info_cmd:ignore-migrations=true"
+    fi
+    if docker exec "$container_name" asinfo $user_credentials -v $cluster_stable_info_cmd 2>&1 | (! grep -qE "^ERROR"); then
         echo "Server is in a stable state."
         break
     fi
