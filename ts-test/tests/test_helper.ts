3e1a6cd2eb70edf8cb1ecc4e88a770c617b522ac
Dominic Pelini <dpelini@aerospike.com>
2024-12-22 23:42:20 -0700

    Skipped functionality tests on older releases

--- ts-test/tests/test_helper.ts
+++ ts-test/tests/test_helper.ts
@@ -243,11 +243,11 @@ Aerospike.setDefaultLogging(config.log ?? {})
 
 
   export function skipUnlessVersionAndEnterprise (this: any, versionRange: any, ctx: Suite) {
-    skipUnless(ctx, () => {return this.cluster.isVersionInRange(versionRange) && this.cluster.isEnterprise() }, `cluster version does not meet requirements: "${versionRange} and/or requires enterprise"`)
+    skipUnless(ctx, () => {return (this.cluster.isVersionInRange(versionRange) && (this.cluster.isEnterprise())) }, `cluster version does not meet requirements: "${versionRange} and/or requires enterprise"`)
   }
 
   export function skipUnlessVersionAndCommunity (this: any, versionRange: any, ctx: Suite) {
-    skipUnless(ctx, () => {return this.cluster.isVersionInRange(versionRange) && (!this.cluster.isEnterprise()) }, `cluster version does not meet requirements: "${versionRange} and/or requires enterprise"`)
+    skipUnless(ctx, () => {return (this.cluster.isVersionInRange(versionRange) && (!this.cluster.isEnterprise())) }, `cluster version does not meet requirements: "${versionRange} and/or requires enterprise"`)
   }
 
   export function skipUnlessSupportsTtl(this: any, ctx: Suite) {
