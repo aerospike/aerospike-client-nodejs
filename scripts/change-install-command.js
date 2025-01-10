6a8cef0bfa06c6ec394594954059de90a3d6e03e
Dominic Pelini <dpelini@aerospike.com>
2025-01-10 10:49:20 -0700

    CI/CD Improvement

--- scripts/change-install-command.js
+++ scripts/change-install-command.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
+const fs = require('fs')
 
 // Path to package.json
-const packageJsonPath = './package.json';
+const packageJsonPath = './package.json'
 
 // Read and parse package.json
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
 
 // Update the install script
-packageJson.scripts.install = "npm-run-all removeExtraBinaries build";
+packageJson.scripts.install = 'npm-run-all removeExtraBinaries build'
 
 // Write the updated package.json back
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8')
 
-console.log('Updated the install script in package.json successfully.');
+console.log('Updated the install script in package.json successfully.')
