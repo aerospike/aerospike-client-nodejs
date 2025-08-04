63fa2dc7b76221c07000daeb0947f40df264b7c2
Dominic Pelini <dpelini@aerospike.com>
2025-08-04 03:29:02 -0600

    Repairing action

--- .github/workflows/bump-dev-version-and-increment-number.js
+++ .github/workflows/bump-dev-version-and-increment-number.js
@@ -23,7 +23,6 @@ if(version.major === jfrogVersion.major &&
       version.prerelease = ['dev', 1];
       console.log(version.format())
    }
-  throw new Error('No new JFrog version exists yet. Please update version in package.json to reflect the next release version.')
 }
 else if(version.major > jfrogVersion.major ||
    version.minor > jfrogVersion.minor ||
