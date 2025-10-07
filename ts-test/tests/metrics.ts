c70b7cb27ce2311974efdc8ea8208f17989a65d4
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2025-10-06 21:09:37 -0600

    Added timeoutDelay CLIENT-3416 (#729)
    * Added timeout delay and timeout delay testing
    
    * Added timeout delay and timeout delay testing
    
    * Removed print statements
    
    * Run dev builds
    
    * updated C Client submodule

--- ts-test/tests/metrics.ts
+++ ts-test/tests/metrics.ts
@@ -1233,6 +1233,8 @@ describe('Metrics tests', function () {
                 expect(node.conns.inPool).to.be.a('number')
                 expect(node.conns.opened).to.be.a('number')
                 expect(node.conns.closed).to.be.a('number')
+                expect(node.conns.recovered).to.be.a('number')
+                expect(node.conns.aborted).to.be.a('number')
               }
             }
 
@@ -2817,7 +2819,7 @@ describe('Metrics tests', function () {
         name: 'A1',
         address: '127.0.0.1',
         port: 3000,
-        conns: { inUse: 0, inPool: 0, opened: 0, closed: 0 },
+        conns: { inUse: 0, inPool: 0, opened: 0, closed: 0, recovered: 0, aborted: 0 },
         metrics
       }
 
