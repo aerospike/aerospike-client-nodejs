83f54c3bdaa52b902a7583224960ea7738d4c18c
Dominic Pelini <dpelini@aerospike.com>
2025-10-08 08:42:03 -0600

    Fixing build issue

--- src/main/commands/enable_metrics.cc
+++ src/main/commands/enable_metrics.cc
@@ -14,6 +14,7 @@
  * limitations under the License.
  ******************************************************************************/
 
+#include <cstdint>
 #include "async.h"
 #include "client.h"
 #include "metrics.h"
