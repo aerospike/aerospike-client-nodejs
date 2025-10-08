4d8d53217ef6a9d28d3d069e418a8e79481cf07a
Dominic Pelini <dpelini@aerospike.com>
2025-10-08 08:13:16 -0600

    Fixing build issue

--- src/include/metrics.h
+++ src/include/metrics.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include "command.h"
 
 class MetricsCommand : public AerospikeCommand {
--- src/main/metrics.cc
+++ src/main/metrics.cc
@@ -14,8 +14,6 @@
  * limitations under the License.
  ******************************************************************************/
 
-
-#include <cstdint>
 #include "async.h"
 #include "metrics.h"
 #include "conversions.h"
