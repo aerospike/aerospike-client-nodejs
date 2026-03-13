b07fe9ef5be81f403887ef878cb05d1db2098bae
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-03-12 19:19:59 -0700

    by default, assume that the next version will be a patch release. We can always override that later. Also since JFrog artifacts should have the anticipated release version, we don't need to compare the jfrog artifact versions with the one in package.json

--- .github/workflows/dev-bump-version.js
+++ .github/workflows/dev-bump-version.js
@@ -8,31 +8,8 @@ if (!jfrogVersion) {
   process.exit(1)
 }
 
-if(version.major === jfrogVersion.major &&
-   version.minor === jfrogVersion.minor &&
-   version.patch === jfrogVersion.patch){
-   if(jfrogVersion.prerelease){
-      jfrogVersion.inc('prerelease', 'dev')
-      console.log(jfrogVersion.format())
-
-   }
-   else{
-      version.prerelease = ['dev', 1];
-      console.log(version.format())
-   }
+if (jfrogVersion.prerelease === false) {
+      jfrogVersion.inc('prepatch')
 }
-else if(version.major > jfrogVersion.major ||
-   version.minor > jfrogVersion.minor ||
-   version.patch > jfrogVersion.patch){
-  //console.log("Current package version higher than current JFrog version.")
-  version.prerelease = ['dev', 1];
-  console.log(version.format())
-}
-else if(version.major < jfrogVersion.major ||
-   version.minor < jfrogVersion.minor ||
-   version.patch < jfrogVersion.patch){
-  //console.log("Current JFrog version higher than current package version.")
-  jfrogVersion.inc('prerelease', 'dev')
-  console.log(jfrogVersion.format())
-
-}
+jfrogVersion.inc('prerelease', 'dev')
+console.log(jfrogVersion.format())
