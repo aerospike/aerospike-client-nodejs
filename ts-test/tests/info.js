4dc33d57835603f8958c759104878437d8e4f51a
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-03-29 09:28:22 -0700

    For the remaining imports in the .js files in ts-test/tests/, fix the incorrect relative imports.

--- ts-test/tests/info.js
+++ ts-test/tests/info.js
@@ -19,10 +19,10 @@
 /* global expect, describe, it, context, before */
 /* eslint-disable no-unused-expressions */
 
-const Aerospike = require('../lib/aerospike')
-const info = require('../lib/info')
+const Aerospike = require('../../lib/aerospike')
+const info = require('../../lib/info')
 const helper = require('./test_helper')
-const utils = require('../lib/utils')
+const utils = require('../../lib/utils')
 
 const AerospikeError = Aerospike.AerospikeError
 
