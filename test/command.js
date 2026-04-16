964eaf3a4b3a2388db239591a137c3faf96deb48
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-16 09:20:41 -0700

    [CLIENT-4003] Add support for bin projection. Also compile and run typescript tests on-the-fly (#1003)
    Extra Changes
    
    # CI/CD
    - Add smoke tests that builds and runs all the dev tests for the node.js client.
    - Allow dev-build-node-cpp-addons.yml to be run manually so we can test all supported platforms on a feature branch.
    
    # Test changes
    - Move ts-test folder containing the Typescript tests to test folder and the test folder containing the Javascript tests to old-test. It seems like the Javascript tests haven't been maintained for a while.
    - Move test dependencies from ts-test/package.json to the root-level package.json. I believe it's simpler to have a single package.json with all the development and test-related dependencies
    - Compile typescript tests on the fly so that we don't have to save the compiled javascript test files on disk in order to run them.
    - In the typescript tests, replace importing the node.js client with relative paths. I believe this is more accurate such that aerospike doesn't need to be installed in the typescript test directory's node_modules folder. Also with this approach, we don't need to have a package.json for the typescript test folder that tells where to find `aerospike`.
    
    Remove tsconfig.tsbuildinfo since this is just for caching the typescript tests when compiling them.
    
    Known issues
    
    - [CLIENT-4648] Foreground scans are unable to perform bin projection. This is ok for now as long as foreground queries fully support bin projection.

--- test/command.js
+++ test/command.js
@@ -19,7 +19,7 @@
 /* eslint-env mocha */
 /* global expect */
 
-require('./test_helper')
+require('./test_helper.ts')
 const Command = require('../lib/commands/command')
 
 describe('Command', function () {
